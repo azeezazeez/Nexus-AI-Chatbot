db2d10c34db8bd7678f6facff01ff73492a9d5bb
ABDUL AZEEZ <azeezazeez7989@gmail.com>
2026-05-12 12:11:22 +0530

    Create FileResponseDto.java

---
+++ Nexus AI-backend/src/main/java/com/ai/chatbot_backend/dto/FileResponseDto.java
@@ -0,0 +1,15 @@
+package com.nexus.api.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FileResponseDto {
+    private Long id;
+    private String filename;
+    private String originalName;
+    private String mimetype;
+    private Long size;
+    private String path;
+}
